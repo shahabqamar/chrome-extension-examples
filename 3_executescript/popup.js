b417f1e919363ac336076495122565b2a252e876
Shahab Qamar <sqamar@squiz.net>
2017-08-10 13:15:28 +1000

    added execute file example

--- 3_executescript/popup.js
+++ 3_executescript/popup.js
@@ -1,9 +1,18 @@
 $(function() {
 
+    //btn click handler
     $("#exec_script").click(function() {
 
+        //executescript on host page using inline JS
         chrome.tabs.executeScript(null, {
             code: 'document.title = "' + $("#your_name").val() + '";'
+        });
+
+        //executescript on host page using a js file
+        chrome.tabs.executeScript(null, {
+            file: "script.js"
+        }, function(result) {
+            console.log(result);
         });
     })
 
---
+++ 3_executescript/script.js
@@ -0,0 +1,3 @@
+console.log("Execute scrript ran from script.js");
+var page_title = document.title;
+page_title
